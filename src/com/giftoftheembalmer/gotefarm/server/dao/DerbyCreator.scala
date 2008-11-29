168e4e1b2debb73d6cd967d44dd328d55e6b70fd
J Robert Ray <jrobertray@gmail.com>
2008-11-29 03:02:29 -0800

    Set Derby system path based on new gotefarm.properties file.
    Get rid of SQLWarnings for each jdbc connection about the database already
    existing.
    
    * build.properties.txt,
      build.xml:
      Need path to Derby jar to compile now.
    
    * gotefarm.properties.sample: Sample properties file now required to set
      the path to where Derby will create its databases.
    
    * .../gotefarm/server/dao/DerbyCreator.scala: New class to open the Derby once
      with create=true to get it to create the gotefarm database if necessary.
      Clear warnings, since Derby warns if the database already exists (probably
      not needed since this connection is not wrapped by c3p0 so the warning
      wouldn't be logged anyway).
    
    * web/WEB-INF/applicationContext.xml
      (property-placeholder): Load up the gotefarm.properties properties for
      substitutions in the context file.
    
      (sysprops): New bean to give access to the system properties object.
    
      (derbysystemhome): New bean to set the derby.system.home property with the
      value from gotefarm.properties.
    
      (createDerbyDatabase): New bean using DerbyCreator to create the Derby
      database, if necessary.
    
      (dataSource): Depend on createDerbyDatabase so create=true can be dropped
      from jdbcUrl to eliminate warning spam.
      Use bare "gotefarm" for database name, since derby.system.home is now being
      set and specifies the directory where the database will be made.

---
+++ src/com/giftoftheembalmer/gotefarm/server/dao/DerbyCreator.scala
@@ -0,0 +1,25 @@
+package com.giftoftheembalmer.gotefarm.server.dao
+
+class DerbyCreator {
+  @scala.reflect.BeanProperty
+  private var driverClass: String = _
+
+  @scala.reflect.BeanProperty
+  private var jdbcUrl: String = _
+
+  def initialize() = {
+    // load the derby driver
+    val driver = new org.apache.derby.jdbc.EmbeddedDriver
+
+    // open a connection to the datasource, causing derby to create the
+    // database if necessary
+    val conn = driver.connect(jdbcUrl, null)
+    try {
+      // suppress warnings
+      conn.clearWarnings()
+    }
+    finally {
+      conn.close()
+    }
+  }
+}
