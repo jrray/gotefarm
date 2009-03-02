14d5c26f535db7b9683bfb4f6720ee9f0dce68ba
J Robert Ray <jrobertray@gmail.com>
2009-03-02 02:16:05 -0800

    Add hasRole method to JSCharacter.
    * .../gotefarm/client/JSCharacter.java
      (hasRole): Return true if the character has the given role (and it has been
      approved).

--- src/com/giftoftheembalmer/gotefarm/client/JSCharacter.java
+++ src/com/giftoftheembalmer/gotefarm/client/JSCharacter.java
@@ -29,4 +29,24 @@ public class JSCharacter implements IsSerializable {
 
         return false;
     }
+
+    public boolean hasRole(long roleid) {
+        for (JSChrRole role : roles) {
+            if (role.roleid == roleid) {
+                return role.approved;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasRole(String name) {
+        for (JSChrRole role : roles) {
+            if (role.name.equals(name)) {
+                return role.approved;
+            }
+        }
+
+        return false;
+    }
 }
