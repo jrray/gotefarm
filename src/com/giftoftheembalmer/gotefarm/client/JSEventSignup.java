3ce004e56b7e161c82a41cc8e041bc1c1c3f0011
J Robert Ray <jrobertray@gmail.com>
2009-03-02 01:37:35 -0800

    Add signup type constants to JSEventSignup.
    * .../gotefarm/client/JSEventSignup.java
      (JSEventSignup): Add static members for signup types and label strings.

--- src/com/giftoftheembalmer/gotefarm/client/JSEventSignup.java
+++ src/com/giftoftheembalmer/gotefarm/client/JSEventSignup.java
@@ -5,6 +5,13 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 import java.util.Date;
 
 public class JSEventSignup implements IsSerializable {
+    static final int SIGNUP_TYPE_COMING = 1;
+    static final int SIGNUP_TYPE_MAYBE = 2;
+    static final int SIGNUP_TYPE_NOT_COMING = 3;
+    static final String SIGNUP_LABEL_COMING = "Coming";
+    static final String SIGNUP_LABEL_MAYBE = "Maybe";
+    static final String SIGNUP_LABEL_NOT_COMING = "Not Coming";
+
     public long eventsignupid;
     public long eventid;
     public JSCharacter chr;
