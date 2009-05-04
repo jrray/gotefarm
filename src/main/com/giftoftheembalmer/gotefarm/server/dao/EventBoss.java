f5b7f33195f346d10a50e299804d093ea2349a60
J Robert Ray <jrobertray@gmail.com>
2009-05-04 04:33:13 -0700

    Jdo model for event bosses.
    * .../gotefarm/server/dao/EventBoss.java
      (EventBoss): New class.

---
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/EventBoss.java
@@ -0,0 +1,44 @@
+package com.giftoftheembalmer.gotefarm.server.dao;
+
+import com.google.appengine.api.datastore.Key;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class EventBoss {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private String boss;
+
+    @Persistent
+    private Key bossKey;
+
+    public EventBoss(String boss, Key bossKey) {
+        this.boss = boss;
+        this.bossKey = bossKey;
+    }
+
+    public String getBoss() {
+        return boss;
+    }
+
+    public Key getBossKey() {
+        return bossKey;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public void setBoss(String boss, Key bossKey) {
+        this.boss = boss;
+        this.bossKey = bossKey;
+    }
+}
