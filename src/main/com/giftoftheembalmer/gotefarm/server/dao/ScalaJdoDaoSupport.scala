b12aa74fdcb87b24baff48001533ff66335af639
J Robert Ray <jrobertray@gmail.com>
2009-05-04 04:29:43 -0700

    Extend JdoDaoSupport to make it easier to use from scala.
    * .../gotefarm/server/dao/ScalaJdoDaoSupport.scala
      (find): Return a properly typed Collection based on parameter.
      Use currying for the `values` parameter so the different flavors of find are
      not ambiguous.
      (getObjectById): Return an Option to be scala friendly.

---
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/ScalaJdoDaoSupport.scala
@@ -0,0 +1,35 @@
+package com.giftoftheembalmer.gotefarm.server.dao
+
+import org.springframework.orm.jdo.support.JdoDaoSupport
+import org.springframework.orm.ObjectRetrievalFailureException
+
+import java.util.Collection
+
+trait ScalaJdoDaoSupport extends JdoDaoSupport {
+  def find[T](entityClass: Class[T]) = {
+    getJdoTemplate.find(entityClass).asInstanceOf[Collection[T]]
+  }
+
+  def find[T](entityClass: Class[T], filter: String, parameters: String)
+             (values: AnyRef*): Collection[T] = {
+    getJdoTemplate.find(entityClass, filter, parameters,
+                        values.toArray).asInstanceOf[Collection[T]]
+  }
+
+  def find[T](entityClass: Class[T], filter: String, parameters: String,
+              ordering: String)
+             (values: AnyRef*): Collection[T] = {
+    getJdoTemplate.find(entityClass, filter, parameters,
+                        values.toArray, ordering).asInstanceOf[Collection[T]]
+  }
+
+  def getObjectById[T](entityClass: Class[T], idValue: AnyRef): Option[T] = {
+    try {
+      Some(getJdoTemplate.getObjectById(entityClass, idValue).asInstanceOf[T])
+    }
+    catch {
+      case _: ObjectRetrievalFailureException =>
+        None
+    }
+  }
+}
