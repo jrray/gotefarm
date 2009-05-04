1ca7efc458efc5af3d67ee0ffd555d5256479a35
J Robert Ray <jrobertray@gmail.com>
2009-05-04 04:32:35 -0700

    Allow transaction template code to throw checked exceptions. Throwing checked exceptions causes the transaction to not be rolled back, and it also leaves a transaction open in the thread, leading to datastore errors.
    Catch all exceptions and rethrow checked exceptions wrapped inside an
    unchecked exception. This allows the transaction to unwind correctly. After
    the transaction is cleaned up, rethrow the original exception.
    
    * .../server/dao/ScalaTransactionTemplate.scala
      (RepackagedCheckedException): New exception type to wrap checked exceptions.
      (ScalaTransactionTemplate.isChecked): Return if a Throwable is a checked
      exception.
      (execute): Wrap super.execute in a try block, catching
      RepackagedCheckedException and throwing the wrapped exception.
      Wrap the Function0 in a try block, catching all exceptions and repackaging
      any checked exceptions in a RepackagedCheckedException, and then re-throw.

--- src/main/com/giftoftheembalmer/gotefarm/server/dao/ScalaTransactionTemplate.scala
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/ScalaTransactionTemplate.scala
@@ -6,34 +6,77 @@ import org.springframework.transaction.support.{
 }
 import org.springframework.transaction.TransactionStatus
 
+final private class RepackagedCheckedException(val e: Throwable)
+  extends RuntimeException(e) {
+  // don't bother filling in the stack trace here, only care about the
+  // repackaged exception
+  override def fillInStackTrace(): Throwable = this
+}
+
 class ScalaTransactionTemplate extends TransactionTemplate {
+  private def isChecked(e: Throwable): Boolean = {
+    !(e.isInstanceOf[RuntimeException] || e.isInstanceOf[Error])
+  }
+
   def execute[T <: AnyRef](f: => T): T = {
-    super.execute(new TransactionCallback {
-      def doInTransaction(status: TransactionStatus) = {
-        try {
-          f
+    try {
+      super.execute(new TransactionCallback {
+        def doInTransaction(status: TransactionStatus) = {
+          try {
+            f
+          }
+          catch {
+            case e: Throwable =>
+              status.setRollbackOnly()
+
+              // May only throw unchecked exceptions here, or else the
+              // transaction will not be rolled back, or cleaned up at all.
+              // Repackage the exception as an unchecked exception if
+              // necessary.
+              if (isChecked(e)) {
+                throw new RepackagedCheckedException(e)
+              }
+              else {
+                throw e
+              }
+          }
         }
-        catch {
-          case e: Throwable =>
-            status.setRollbackOnly()
-            throw e
-        }
-      }
-    }).asInstanceOf[T]
+      }).asInstanceOf[T]
+    }
+    catch {
+      case e: RepackagedCheckedException =>
+        throw e.e
+    }
   }
 
   def execute(f: => Unit): Unit = {
-    super.execute(new TransactionCallback {
-      def doInTransaction(status: TransactionStatus) = {
-        try {
-          f
+    try {
+      super.execute(new TransactionCallback {
+        def doInTransaction(status: TransactionStatus) = {
+          try {
+            f
+          }
+          catch {
+            case e: Throwable =>
+              status.setRollbackOnly()
+
+              // May only throw unchecked exceptions here, or else the
+              // transaction will not be rolled back, or cleaned up at all.
+              // Repackage the exception as an unchecked exception if
+              // necessary.
+              if (isChecked(e)) {
+                throw new RepackagedCheckedException(e)
+              }
+              else {
+                throw e
+              }
+          }
         }
-        catch {
-          case e: Throwable =>
-            status.setRollbackOnly()
-            throw e
-        }
-      }
-    })
+      })
+    }
+    catch {
+      case e: RepackagedCheckedException =>
+        throw e.e
+    }
   }
 }
