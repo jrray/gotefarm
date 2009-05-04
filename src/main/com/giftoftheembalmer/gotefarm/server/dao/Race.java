e6d12707a1eca61c5a58cc8f68a4593a58ccbd70
J Robert Ray <jrobertray@gmail.com>
2009-05-04 04:29:27 -0700

    Jdo model for character races.
    * .../gotefarm/server/dao/Race.java
      (Race): New class.

---
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/Race.java
@@ -0,0 +1,35 @@
+package com.giftoftheembalmer.gotefarm.server.dao;
+
+import com.google.appengine.api.datastore.Key;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Race {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private String name;
+
+    public Race(String name) {
+        this.name = name;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
