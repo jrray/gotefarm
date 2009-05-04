8ad6157d6802718e87c8d97e88c1e6b07940c21d
J Robert Ray <jrobertray@gmail.com>
2009-05-04 04:29:13 -0700

    Jdo model for characters.
    * .../giftoftheembalmer/gotefarm/server/dao/Chr.java
      (Chr): New class.

---
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/Chr.java
@@ -0,0 +1,120 @@
+package com.giftoftheembalmer.gotefarm.server.dao;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Text;
+import com.google.appengine.api.users.User;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import java.util.Date;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Chr {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private User user;
+
+    @Persistent
+    private String realm;
+
+    @Persistent
+    private String name;
+
+    @Persistent
+    private Key race;
+
+    @Persistent
+    private Key clazz;
+
+    @Persistent
+    private int level;
+
+    @Persistent
+    private Text chrxml;
+
+    @Persistent
+    private Date created;
+
+    public Chr(User user, String realm, String name, Key race, Key clazz,
+               int level, String chrxml, Date created) {
+        this.user = user;
+        this.realm = realm;
+        this.name = name;
+        this.race = race;
+        this.clazz = clazz;
+        this.level = level;
+        this.chrxml = new Text(chrxml);
+        this.created = created;
+    }
+
+    public Key getChrClass() {
+        return clazz;
+    }
+
+    public String getChrXml() {
+        return chrxml.getValue();
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Key getRace() {
+        return race;
+    }
+
+    public String getRealm() {
+        return realm;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setChrClass(Key clazz) {
+        this.clazz = clazz;
+    }
+
+    public void setChrXml(String chrxml) {
+        this.chrxml = new Text(chrxml);
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRace(Key race) {
+        this.race = race;
+    }
+
+    public void setRealm(String realm) {
+        this.realm = realm;
+    }
+}
