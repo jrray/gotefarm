bba78c4a73065adc180804795ee98de43c65ddf5
J Robert Ray <jrobertray@gmail.com>
2009-05-04 04:29:58 -0700

    Jdo model for guilds, somewhat a misnomer, but the "owner" of events and characters.
    * .../gotefarm/server/dao/Guild.java
      (Guild): New class.

---
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/Guild.java
@@ -0,0 +1,87 @@
+package com.giftoftheembalmer.gotefarm.server.dao;
+
+import com.google.appengine.api.datastore.Key;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Guild {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private String region;
+
+    @Persistent
+    private String realm;
+
+    @Persistent
+    private String name;
+
+    @Persistent
+    private Key owner;
+
+    @Persistent
+    private Set<Key> officers;
+
+    @Persistent
+    private Date lastUpdate;
+
+    public Guild(String region, String realm, String name, Key owner) {
+        this.region = region;
+        this.realm = realm;
+        this.name = name;
+        this.owner = owner;
+        officers = new HashSet<Key>();
+        lastUpdate = new Date();
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Key> getOfficers() {
+        return officers;
+    }
+
+    public Key getOwner() {
+        return owner;
+    }
+
+    public String getRealm() {
+        return realm;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwner(Key owner) {
+        this.owner = owner;
+    }
+}
