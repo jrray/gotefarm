b1abd32411439f599eb70d6648879da04c4b8360
J Robert Ray <jrobertray@gmail.com>
2009-05-07 06:15:19 -0700

    Jdo model for a class to own all the characters of a user within a single guild, and to track that user's "main" character per guild.
    * .../gotefarm/server/dao/Account.java
      (Account): Create owned relationship with ChrGroup.
    
    * .../gotefarm/server/dao/ChrGroup.java
      (ChrGroup): New class.

--- src/main/com/giftoftheembalmer/gotefarm/server/dao/Account.java
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/Account.java
@@ -3,13 +3,18 @@ package com.giftoftheembalmer.gotefarm.server.dao;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.users.User;
 
+import javax.jdo.annotations.Element;
+import javax.jdo.annotations.Extension;
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Order;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @PersistenceCapable(identityType = IdentityType.APPLICATION)
@@ -30,6 +35,14 @@ public class Account {
     @Persistent
     private Date lastUpdate;
 
+    // FIXME: Using List here due to AppEngine bug, switch to Set once
+    // it is fixed.
+    // http://code.google.com/p/datanucleus-appengine/issues/detail?id=26
+    @Persistent(mappedBy = "account")
+    @Element(dependent = "true")
+    @Order(extensions = @Extension(vendorName="datanucleus", key="list-ordering", value="main asc"))
+    private List<ChrGroup> chrGroups = new ArrayList<ChrGroup>();
+
     public Account(User user) {
         this.user = user;
         lastUpdate = new Date();
@@ -37,6 +50,10 @@ public class Account {
 
     public Key getActiveGuild() {
         return activeGuild;
+    }
+
+    public List<ChrGroup> getChrGroups() {
+        return chrGroups;
     }
 
     public Set<Key> getGuilds() {
@@ -57,6 +74,10 @@ public class Account {
 
     public void setActiveGuild(Key activeGuild) {
         this.activeGuild = activeGuild;
+    }
+
+    public void setChrGroups(List<ChrGroup> chrGroups) {
+        this.chrGroups = chrGroups;
     }
 
     public void setGuilds(Set<Key> guilds) {
---
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/ChrGroup.java
@@ -0,0 +1,77 @@
+package com.giftoftheembalmer.gotefarm.server.dao;
+
+import com.google.appengine.api.datastore.Key;
+
+import javax.jdo.annotations.Element;
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Order;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class ChrGroup {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private Account account;
+
+    @Persistent
+    private Key guildKey;
+
+    @Persistent
+    @Element(dependent = "true")
+    @Order(extensions = @Extension(vendorName="datanucleus", key="list-ordering", value="name asc"))
+    private List<Chr> characters = new ArrayList<Chr>();
+
+    @Persistent
+    private String main;
+
+    @Persistent
+    private Key mainKey;
+
+    public ChrGroup(Account account, Key guildKey) {
+        this.account = account;
+        this.guildKey = guildKey;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public List<Chr> getCharacters() {
+        return characters;
+    }
+
+    public Key getGuildKey() {
+        return guildKey;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public Key getMainKey() {
+        return mainKey;
+    }
+
+    public void setCharacters(List<Chr> characters) {
+        this.characters = characters;
+    }
+
+    public void setMain(String main, Key mainKey) {
+        this.main = main;
+        this.mainKey = mainKey;
+    }
+}
