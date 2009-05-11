d7089565f20c41ed7178698f5c4315f8b0530437
J Robert Ray <jrobertray@gmail.com>
2009-05-11 00:32:28 -0700

    Maintain display/signups start/end as Dates so they can be queried on and ordered by without involving math in the query.
    * .../gotefarm/server/dao/EventSchedule.java
      (displayStartDate, displayEndDate, signupsStartDate, signupsEndDate): New
      members and getters.
      (EventSchedule): Set display/signups start/end through setters so the Date
      counterparts are computed.
      (calcDate): Routine to compute a new Date as an offset in seconds from
      a source Date.
      (setDisplayStart, setDisplayEnd, setSignupsStart, setSignupsEnd): Also
      compute the Date counterpart when any of these change.
      (setStartTime): Must recompute all Date counterparts when the start time
      changes.

--- src/main/com/giftoftheembalmer/gotefarm/server/dao/EventSchedule.java
+++ src/main/com/giftoftheembalmer/gotefarm/server/dao/EventSchedule.java
@@ -38,13 +38,25 @@ public class EventSchedule {
     private int displayStart;
 
     @Persistent
+    private Date displayStartDate;
+
+    @Persistent
     private int displayEnd;
+
+    @Persistent
+    private Date displayEndDate;
 
     @Persistent
     private int signupsStart;
 
     @Persistent
+    private Date signupsStartDate;
+
+    @Persistent
     private int signupsEnd;
+
+    @Persistent
+    private Date signupsEndDate;
 
     @Persistent
     private int repeatSize;
@@ -72,15 +84,19 @@ public class EventSchedule {
         this.origStartTime = startTime;
         this.timeZone = timeZone;
         this.duration = duration;
-        this.displayStart = displayStart;
-        this.displayEnd = displayEnd;
-        this.signupsStart = signupsStart;
-        this.signupsEnd = signupsEnd;
+        setDisplayStart(displayStart);
+        setDisplayEnd(displayEnd);
+        setSignupsStart(signupsStart);
+        setSignupsEnd(signupsEnd);
         this.repeatSize = repeatSize;
         this.repeatFreq = repeatFreq;
         this.dayMask = dayMask;
         this.repeatBy = repeatBy;
         this.active = active;
+    }
+
+    private Date calcDate(Date from, int secondsDelta) {
+        return new Date(from.getTime() + secondsDelta * 1000L);
     }
 
     public int getDayMask() {
@@ -91,8 +107,16 @@ public class EventSchedule {
         return displayEnd;
     }
 
+    public Date getDisplayEndDate() {
+        return displayEndDate;
+    }
+
     public int getDisplayStart() {
         return displayStart;
+    }
+
+    public Date getDisplayStartDate() {
+        return displayStartDate;
     }
 
     public int getDuration() {
@@ -131,8 +155,16 @@ public class EventSchedule {
         return signupsEnd;
     }
 
+    public Date getSignupsEndDate() {
+        return signupsEndDate;
+    }
+
     public int getSignupsStart() {
         return signupsStart;
+    }
+
+    public Date getSignupsStartDate() {
+        return signupsStartDate;
     }
 
     public Date getStartTime() {
@@ -157,10 +189,12 @@ public class EventSchedule {
 
     public void setDisplayEnd(int displayEnd) {
         this.displayEnd = displayEnd;
+        this.displayEndDate = calcDate(startTime, displayEnd);
     }
 
     public void setDisplayStart(int displayStart) {
         this.displayStart = displayStart;
+        this.displayStartDate = calcDate(startTime, -displayStart);
     }
 
     public void setDuration(int duration) {
@@ -185,14 +219,26 @@ public class EventSchedule {
 
     public void setSignupsEnd(int signupsEnd) {
         this.signupsEnd = signupsEnd;
+        this.signupsEndDate = calcDate(startTime, -signupsEnd);
     }
 
     public void setSignupsStart(int signupsStart) {
         this.signupsStart = signupsStart;
+        this.signupsStartDate = calcDate(startTime, -signupsStart);
     }
 
     public void setStartTime(Date startTime) {
+        if (this.startTime.equals(startTime)) {
+            return;
+        }
+
         this.startTime = startTime;
+
+        // Update new display/signup start/end Dates
+        setDisplayStart(displayStart);
+        setDisplayEnd(displayEnd);
+        setSignupsStart(signupsStart);
+        setSignupsEnd(signupsEnd);
     }
 
     public void setTimeZone(String timeZone) {
